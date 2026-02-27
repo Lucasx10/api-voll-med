247dd8afe8328e2c47aa85b200534fde3b420010
lucas <lukasprado260@gmail.com>
2026-02-27 18:34:25 -0400

    Adiciona endpoint GET /pacientes com paginação

--- src/main/java/med/voll/api/paciente/DadosPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosPaciente.java
@@ -1,7 +1,9 @@
 package med.voll.api.paciente;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import med.voll.api.endereco.DadosEndereco;
 
@@ -16,6 +18,6 @@ public record DadosPaciente(
         @NotBlank
         @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
         String cpf,
-        @NotBlank
+        @NotNull @Valid
         DadosEndereco endereco ) {
 }
